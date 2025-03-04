e2828f1f892672e5e1f14adb69af607d846cfb37
rajea-bilal <rajea.bilal@gmail.com>
2025-03-04 14:37:50 +0000

    made chat interface responsove to mobile screens

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -9,7 +9,7 @@ export default function ChatPage() {
     <main className="relative flex-grow min-h-0 z-20 px-14 pt-8 pb-20">
       <Header />
       <ChatProvider>
-        <div className=" w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[350px,1fr] grid-rows-[auto,1fr] md:grid-rows-none h-[85vh] md:h-full flex-grow min-h-0 gap-7 pt-20">
+        <div className=" w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[200px,1fr] lg:grid-cols-[350px,1fr] grid-rows-[auto,1fr] md:grid-rows-none h-[85vh] md:h-full flex-grow min-h-0 gap-7 pt-20">
           <ChatListContainer />
           <ChatContainer />
         </div>
